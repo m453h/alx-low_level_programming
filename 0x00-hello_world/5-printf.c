af356f3488f0144d9bc74a5820269e5d4aa2be8d
Michael Hudson <mnkotagu@gmail.com>
2022-12-08 19:42:18 +0300

    Implement 5-printf.c c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints a message using printf
+*
+* Description: the main function
+* this function prints with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
