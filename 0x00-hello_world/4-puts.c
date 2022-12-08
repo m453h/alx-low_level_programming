10487d27fb50d4c4e3ce819c7bf7f2e220810100
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2022-12-08 08:20:56 -0800

    Improve 4-puts.c script to comply to Betty style programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-int main()
+/**
+ * main - prints a message using puts
+ *
+ * Description: this function prints the message
+ * "Programming is like building a multilingual puzzle
+ * 
+ * Return: 0 if execution is successful
+ */
+int main(void)
 {	
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
