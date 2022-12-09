052cde4a7fa9bb9d867d2744ec9831ee361a6f03
Michael Hudson <mnkotagu@gmail.com>
2022-12-09 16:48:11 +0300

    Implement 3-print_alphabets.c source file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints alphabet in lowercase followed by new line
+*
+* Description: using the main function this program
+* prints the alphabet in lowecase, followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	char lowercase_alphabet;
+	char uppercase_alphabet;
+
+	lowercase_alphabet = 'a';
+	uppercase_alphabet = 'A';
+
+	while (lowercase_alphabet < 123)
+	{
+		putchar(lowercase_alphabet);
+		lowercase_alphabet++;
+	}
+
+	while (uppercase_alphabet < 91)
+	{
+		putchar(uppercase_alphabet);
+		uppercase_alphabet++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
