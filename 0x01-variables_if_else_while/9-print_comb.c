206d2c92a620b44d155cea4dba315130978bc770
Michael Hudson <mnkotagu@gmail.com>
2022-12-09 17:57:42 +0300

    Implement 9-print_comb.c source file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of single-digit numbers
+*
+* Description: using the main function this program
+* prints all possible combinations of single-digit numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	number = 0;
+
+	while (number < 10)
+	{
+		putchar(number % 10 + '0');
+		number++;
+
+		if (number < 10)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
