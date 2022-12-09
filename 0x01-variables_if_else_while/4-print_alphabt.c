b4315e6c2496a654e5886fbd7bd49ee8b35ef02d
Michael Hudson <mnkotagu@gmail.com>
2022-12-09 16:58:31 +0300

    Implement 4-print_alphabt.c source file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints alphabet in lowercase except 'q' and 'e' followed by a new line
+*
+* Description: using the main function this program
+* prints the alphabet in lowecase, except 'q' and 'e' followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet < 123)
+	{
+		if(alphabet != 101 && alphabet != 113)
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
