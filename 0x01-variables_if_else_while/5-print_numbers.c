1302229967a90e1cceeeb7c905e2559ff5dee3b0
Michael Hudson <mnkotagu@gmail.com>
2022-12-09 17:07:12 +0300

    Implement 5-print_numbers.c script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prints single digit numbers of base 10 followed by new line
+*
+* Description: using the main function this program
+* prints single digit numbers of base 10, followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	number = 0;
+
+	while (number < 10)
+	{
+		printf("%i",number);
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
