34018bb9e10592228e345859deee48e0dd321d7b
Michael Hudson <mnkotagu@gmail.com>
2022-12-13 20:23:19 +0300

    Implement 6-abs.c source file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: The input integer to display absolute value
+ *
+ * Return: absolute integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
