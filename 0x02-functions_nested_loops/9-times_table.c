2eadf33db1f282fdcebf596022bc470f25045ea1
Michael Hudson <mnkotagu@gmail.com>
2022-12-13 21:42:44 +0300

    Implement 9-times_table.c source file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * display_multiplication_result - Displays result of multiplying two numbers
+ * @i: first input number to be multiplied
+ * @j: second input number
+ *
+ * Return: (void)
+ */
+void display_multiplication_result(int i, int j)
+{
+	int first_digit;
+	int last_digit;
+	int number;
+
+	number = i * j;
+	first_digit = number / 10;
+	last_digit = number % 10;
+
+	if (first_digit != 0)
+	{
+		_putchar(first_digit + '0');
+	}
+	else
+	{
+		_putchar(' ');
+	}
+
+	_putchar(last_digit + '0');
+}
+
+/**
+ * times_table - prints the 9 multiplication table
+ *
+ * Return: (void)
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			display_multiplication_result(i, j);
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
