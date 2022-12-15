1e2f7439c366838031ffd8b390e379ade0395b4d
Michael Hudson <mnkotagu@gmail.com>
2022-12-15 21:47:17 +0300

    Implement 100-prime_factor.c source file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include "math.h"
+
+/**
+ * is_prime - checks if a number is prime
+ * @n: The number to check if is prime
+ *
+ * Return: 1 if number is prime, otherwise 0
+ */
+int is_prime(long int n)
+{
+	int i;
+
+	for (i = n - 1; i > 1; i--)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+
+
+/**
+ * main - prints largest prime factor of a number
+ *
+ * description - using the main function this program
+ * prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int number, i;
+
+	number = 612852475143;
+	i = sqrt(number);
+
+	while (i > 1)
+	{
+		if (number % i == 0 && is_prime(i) == 1)
+		{
+			printf("%ld", i);
+			break;
+		}
+
+		i--;
+	}
+
+	printf("\n");
+
+	return (0);
+}
