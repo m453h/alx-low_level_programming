a6795b18003a61b1f067ab750d154a712b623bb5
Michael Hudson <mnkotagu@gmail.com>
2022-12-15 19:45:43 +0300

    Implement 9-fizz-buzz.c source file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * main - prints numbers from 1 to 100
+ * description - using the main function this program
+ * displays numbers from 1 to 100 and displays the word
+ * Fizz for multiples of 3 and the word Buzz for multiples
+ * of 5 and the word FizzBuzz for numbers that are both
+ * multiples of 3 and 5
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
