ee04d7769da4f37b135d516bfb9997cfe237d01c
Michael Hudson <mnkotagu@gmail.com>
2022-12-19 23:50:55 +0300

    Improve 8-print_array.c to comply to Betty Coding Style

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,8 @@
 
 /**
  * print_array - prints all elements of an array of integers
- * @a - array of integers
- * @n - length of array
+ * @a: array of integers
+ * @n: length of array
  *
  *  Return: (void)
  */
@@ -11,11 +11,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d",a[i]);
+		printf("%d", a[i]);
 
-		if(i != n - 1)
+		if (i != n - 1)
 			printf(", ");
 	}
 
