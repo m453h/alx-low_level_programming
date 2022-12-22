feebe5e9cdb98e5a0f7b44de515253dfee0bc250
Michael Hudson <mnkotagu@gmail.com>
2022-12-22 22:23:02 +0300

    Implement 100-rot13.c source file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * rot13 - enocdes a string using rot13
+ * @s: the input string
+ *
+ * Return: (char) the string encoded to 1337
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	char a[] = {
+		'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+
+	char r[] = {
+		'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = r[j];
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (s);
+}
