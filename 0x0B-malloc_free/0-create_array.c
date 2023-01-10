b21f9e9cb698d2732ccd8ced96b1e82398de6e49
Michael Hudson <mnkotagu@gmail.com>
2023-01-10 20:00:10 +0300

    Implement 0-create_array.c source file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array and initializes
+ * it with a certain character
+ *
+ * @c: the array to create
+ * @size: the length of the array to be filled
+ *
+ *  Return: (char *) - pointer to first element of created array
+ */
+char *create_array(unsigned int size, char c)
+{
+
+	char *array;
+	unsigned int i = 0;
+
+	array = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+A repository that contains source code for the 0x0B. C - malloc, free project
