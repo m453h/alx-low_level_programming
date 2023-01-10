3ef63d1c117b81608ab027b82de5b187459e2828
Michael Hudson <mnkotagu@gmail.com>
2023-01-10 20:02:59 +0300

    Improve 0-create_array.c to check for the malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,9 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
 	array = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
