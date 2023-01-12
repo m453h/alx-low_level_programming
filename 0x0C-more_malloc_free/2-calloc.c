d0fadc14c1ba064dd0d31c1312d0cb23da5f7f95
Michael Hudson <mnkotagu@gmail.com>
2023-01-12 18:57:49 +0300

    Implement 2-calloc.c source file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: The number of array elements
+ * @size: The number of bytes for each element
+ *
+ *  Return: (NULL) if @nmemb = 0 or @size = 0,
+ *  pointer to created array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *memb_array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memb_array = malloc(size * nmemb);
+
+	if (memb_array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		memb_array[i] = '0';
+	}
+
+	return (memb_array);
+}
