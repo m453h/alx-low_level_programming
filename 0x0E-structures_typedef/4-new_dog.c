7e757f0e906c0480d8e572bcadfa2a61bd439c54
Michael Hudson <mnkotagu@gmail.com>
2023-01-16 20:14:02 +0300

    Attempt improved free memory dynamically allocated in 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,17 +60,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	d->name = allocate_str(name);
-	d->age = age;
-	d->owner = allocate_str(owner);
 
-	if (d->name == NULL || d->owner == NULL)
+	if (d->name == NULL)
 	{
-		free(d->name);
-		free(d->owner);
 		free(d);
 		return (NULL);
 	}
 
+	d->owner = allocate_str(owner);
+
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->age = age;
 
 	return (d);
 }
