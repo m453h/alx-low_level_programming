9e352f655ab1e1975e40a81ca2f2c0ee1d6df7d5
Michael Hudson <mnkotagu@gmail.com>
2023-01-16 15:49:01 +0300

    Fix 2-print_dog.c source file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,8 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name);
-                printf("Age: %f\n", d->age);
-                printf("Owner: %s\n", d->owner);
+		printf("Age: %f\n", d->age);
+		prtinf("Owner: %s\n", d->owner);
 	}
 }
+
