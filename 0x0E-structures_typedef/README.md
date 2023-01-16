3a5ef280f443df0a6e6afbfa5f196f24d4245eff
Michael Hudson <mnkotagu@gmail.com>
2023-01-16 14:04:44 +0300

    Implement dog.h source file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This folder contains source code for the 0x0E. C - Structures, typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Stores information about a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
