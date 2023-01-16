3d46b46d13fcf860779503cc2904519aec63fc88
Michael Hudson <mnkotagu@gmail.com>
2023-01-16 15:34:44 +0300

    Implement 2-print_dog.c source file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog struct
+ *
+ * @d: pointer to the dog struct to print
+ *
+ * Return: (void)
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+                printf("Age: %f\n", d->age);
+                printf("Owner: %s\n", d->owner);
+	}
+}
