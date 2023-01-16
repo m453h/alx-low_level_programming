4a0d586da8067033ee048cedbbda8223fd141e16
Michael Hudson <mnkotagu@gmail.com>
2023-01-16 15:08:20 +0300

    Implement 1-init_dog.c source file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes dog struct
+ *
+ * @d: pointer to the dog struct to iniialize
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ * Return: (void)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
