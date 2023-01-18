9fed3ca7ebb8170c2651c960c80cb1e259997e9a
Michael Hudson <mnkotagu@gmail.com>
2023-01-18 21:29:02 +0300

    Implement 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h source file(s)

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - performs simple operations
+ *
+ * @argc: the number of arguments passed to the program
+ * @argv: the array containing the arguments passed to the program
+ *
+ * Description: calculates basic arithmetic
+ *
+ * Return: (0) if success,
+ * else if wrong number of arguments passed returns (98),
+ * else if second operand is 0 and the operation is / or % returns (100),
+ * else if incorrect operand has been passed returns (99)
+ */
+int main(int argc, char *argv[])
+{
+	char *op;
+	int a, b;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	op = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (*op == '/' || *op == '%')
+	{
+		if (b == 0)
+		{
+			printf("Error\n");
+			return (100);
+		}
+	}
+
+	op_func = get_op_func(op);
+
+	if (op_func == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	printf("%d\n", op_func(a, b));
+
+	return (0);
+}
