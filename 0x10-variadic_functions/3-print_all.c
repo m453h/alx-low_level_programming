dd7c94b3386dcb462c9bf368f588185076da5f91
Michael Hudson <mnkotagu@gmail.com>
2023-01-19 21:09:51 +0300

    Implement 3-print_all.c source file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints char data type.
+ * @arg: list of characters to print.
+ */
+void print_char(va_list arg)
+{
+	printf("%c", (char) va_arg(arg, int));
+}
+
+/**
+ * print_int - prints int data type.
+ * @arg: list of int to print.
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_float - prints float data type.
+ * @arg: list of float to print
+ */
+void print_float(va_list arg)
+{
+	printf("%f", (float) va_arg(arg, double));
+}
+
+/**
+ * print_string - prints string data type.
+ * @arg: list of string to print
+ */
+void print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
+
+
+/**
+ * print_all - prints float, int, string, char types.
+ * @format: type of data to print.
+ * @...: a variable number of parameters to print.
+ *
+ * Return: (void)
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	va_list list;
+
+	p_t pts[] = {
+		{print_char, 'c'},
+		{print_int, 'i'},
+		{print_float, 'f'},
+		{print_string, 's'}
+	};
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+
+		while (format[i] != pts[j].p && j < 4)
+		{
+			j++;
+		}
+		
+		if (j < 4)
+		{
+			if (i != 0)
+				printf(", ");
+			pts[j].f(list);
+		}
+
+		i++;
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,22 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+/**
+ * struct p - custom data type for selecting appropriate
+ * print function for a given format.
+ * @p: the format to print.
+ * @f: the function associated with @p.
+ */
+typedef struct p
+{
+	void (*f)(va_list arg);
+	char p;
+
+} p_t;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* VARIADIC_FUNCTION_H */
