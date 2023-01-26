b19ebb1edd0c3fa3482c871a6bcc96c851f1e44e
Michael Hudson <mnkotagu@gmail.com>
2023-01-26 22:38:49 +0300

    Implement 1-list_len.c source file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - counts the number of elements in a linked list
+ * @h: the list_t list to count number of elements from.
+ *
+ * Return: number of elements in @h.
+ */
+
+size_t list_len(const list_t *h);
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
