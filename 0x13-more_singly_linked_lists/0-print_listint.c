863043665a911e6ec529418023806ac5e60ea0df
Michael Hudson <mnkotagu@gmail.com>
2023-01-30 23:33:39 +0300

    Implement 0-print_listint.c source file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a listint_t list.
+ * @h: the listint_t list to print.
+ *
+ * Return: number of nodes in h.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		
+		printf("%d\n", h->n);
+
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
