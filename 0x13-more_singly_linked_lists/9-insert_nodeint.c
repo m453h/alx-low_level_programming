2a18f6ab2787f95423cc60e4e22635585981d84a
Michael Hudson <mnkotagu@gmail.com>
2023-01-31 22:21:07 +0300

    Add extra check for adding first item on list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,6 +27,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	iterator = *head;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+
+		return (new_node);
+	}
+
 
 	while (iterator != NULL)
 	{
