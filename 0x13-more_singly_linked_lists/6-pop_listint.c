67825518d580384ff05df4abe5ddc862d7b3b895
Michael Hudson <mnkotagu@gmail.com>
2023-01-31 20:09:12 +0300

    Implement 6-pop_listint.c source file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ *
+ * @head: the listint_t head to delete.
+ *
+ * Return: (int) returns the head node’s data (n), else (0) if node
+ * is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *iterator;
+
+	if (head == NULL)
+		return (0);
+
+	iterator = (*head);
+	*head = iterator->next;
+
+	return (iterator->n);
+}
