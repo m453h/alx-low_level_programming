727a0b859788938c85b60841d2763cd7a1f4b6d7
Michael Hudson <mnkotagu@gmail.com>
2023-01-31 22:07:28 +0300

    Fix betty style issues in 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	size_t counter = 0;
 	listint_t *new_node, *iterator;
 
-	iterator = *head; 
+	iterator = *head;
 
 	while (iterator != NULL)
 	{
