0b8a6be766b7573501c43c7f143bc8ce44bcdf65
Michael Hudson <mnkotagu@gmail.com>
2023-01-31 22:22:19 +0300

    Fix betty coding style issues

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,10 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-
 		return (new_node);
 	}
-
 
 	while (iterator != NULL)
 	{
