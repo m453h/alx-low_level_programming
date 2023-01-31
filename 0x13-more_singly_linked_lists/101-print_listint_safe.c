6be90718afbe45aa88617e1bc5602eb92a18a177
Michael Hudson <mnkotagu@gmail.com>
2023-02-01 00:05:13 +0300

    Improve 101-print_listint_safe.c source file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,45 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop -  finds the loop in a linked list.
+ * @head: the listint_t head.
+ *
+ * Return: the address of the node where the loop starts else,
+ * (NULL) if loop not found
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *t_node, *h_node;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	t_node = head->next;
+	h_node = (head->next)->next;
+
+	while (h_node)
+	{
+		if (t_node == h_node)
+		{
+			t_node = head;
+
+			while (t_node != h_node)
+			{
+				t_node = t_node->next;
+				h_node = h_node->next;
+			}
+
+			return (t_node);
+		}
+
+		t_node = t_node->next;
+		h_node = (h_node->next)->next;
+	}
+
+	return (NULL);
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list.
  * @head: the listint_t head.
  *
@@ -8,20 +47,28 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
+	size_t counter, index = 0;
+
+	counter = looped_listint_len(head);
+
+	if (counter == 0)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
+		for (; head != NULL; counter++)
 		{
-			return (1 + print_listint_safe(head->next));
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		else
+	}
+	else
+	{
+		for (index = 0; index < counter; index++)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
-	return (0);
+	return (counter);
 }
