3d2e7fd2c1ee2653e4997737ce2d7a591de3cbb8
Michael Hudson <mnkotagu@gmail.com>
2023-02-01 00:34:06 +0300

    Implement 102-free_listint_safe.c source file

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,95 @@
+#include "lists.h"
+
+/**
+ * looped_listint_len - counts the number of unique counter
+ * in listint_t linked list.
+ * @head: the listint_t head.
+ *
+ * Return: the address of the node where the loop starts else,
+ * (NULL) if loop not found
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *t_node, *h_node;
+	size_t counter = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	t_node = head->next;
+	h_node = (head->next)->next;
+
+	while (h_node)
+	{
+		if (t_node == h_node)
+		{
+			t_node = head;
+
+			while (t_node != h_node)
+			{
+				t_node = t_node->next;
+				h_node = h_node->next;
+				counter++;
+			}
+
+			t_node = t_node->next;
+
+			while (t_node != h_node)
+			{
+				counter++;
+				t_node = t_node->next;
+			}
+
+			return (counter);
+		}
+
+		t_node = t_node->next;
+		h_node = (h_node->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t counter, index;
+
+	counter = looped_listint_count(*h);
+
+	if (counter == 0)
+	{
+		for (; h != NULL && *h != NULL; counter++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < counter; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (counter);
+}
