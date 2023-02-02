ede59c47a3d822d02873f99a526ae14beb46b00a
Michael Hudson <mnkotagu@gmail.com>
2023-02-02 22:50:28 +0300

    Implement 5-flip_bits.c source file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - determines the number of bits you would need
+ * to flip to get from one number to another
+ *
+ * @n: the first number
+ * @m: the second number
+ *
+ * Return: the number of bits that need to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int n_bits = 0;
+	unsigned int x = n ^ m;
+
+	while (x > 0)
+	{
+		n_bits++;
+		x = x & (x - 1);
+	}
+
+	return (n_bits);
+}
