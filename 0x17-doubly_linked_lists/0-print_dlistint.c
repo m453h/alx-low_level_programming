2d777458c46a068780d5bc2a40bed141446afc3c
Michael Hudson <mnkotagu@gmail.com>
2023-03-16 17:28:58 +0300

    Improve 0-print_dlistint.c source file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_dlistint - prints all the elements of a list_t list.
  * @h: the dlistint_t list to print.
  *
  * Return: number of nodes in h.
