dfc8f3853de6cb59cbb89a3ab02e925eb3061c69
Michael Hudson <mnkotagu@gmail.com>
2023-03-16 17:35:47 +0300

    Implement 1-dlistint_len.c source file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * dlistint_len - calculates the length of doubly linked list
+ * @h: the pointer to the head of the dlistint_t list to find length
+ *
+ * Return: (int) number of nodes in doubly linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
