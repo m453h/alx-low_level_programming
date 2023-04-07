a23baecd217e49b2411bc68128f5c3fdd7d079c2
Michael Hudson <mnkotagu@gmail.com>
2023-04-07 21:41:05 +0300

    Implement 4-hash_table_get.c source file

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht:  the hash table to look into
+ * @key: the key to look for
+ *
+ * Return: (char*) - 1 on success ELSE 0
+ *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current_node;
+	unsigned long int index;
+
+	if (key == NULL || key[0] == '\0' || ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			return (current_node->value);
+		}
+		current_node = current_node->next;
+	}
+
+	return (NULL);
+}
