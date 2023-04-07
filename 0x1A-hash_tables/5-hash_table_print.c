5a925082f5f6c21cf6d153998b47ad1a6c67a257
Michael Hudson <mnkotagu@gmail.com>
2023-04-07 22:15:06 +0300

    Improve 5-hash_table_print.c to not print when array is null

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,7 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int size, i;
 	int displayed_pairs = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 
 	printf("{");
