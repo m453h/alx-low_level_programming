aeaf854ec309bd368fcc6a26aae43773d3ff2220
Michael Hudson <mnkotagu@gmail.com>
2023-07-11 22:38:44 +0300

    Improve output of binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		int middle = (left + right) / 2;
 
-		printf("Searching subarray: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
